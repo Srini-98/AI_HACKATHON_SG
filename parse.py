8175c51d031d743187c664003389cf7fc91fe941
Srini-98 <srini481@gmail.com>
2024-06-08 13:26:03 +0800

    JSON mode and SUPA BASE INTEGRATION

--- parse.py
+++ parse.py
@@ -7,7 +7,8 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     pdfReader = PyPDF2.PdfReader(uploaded_file)
     pageObj = pdfReader.pages[0]
-    response = get_response(pageObj.extract_text())
-
+    file_name = uploaded_file.name
+    response = get_response(pageObj.extract_text() , file_name)
     st.write(response)
+    
 #command to launch streamlit
--- splitResume.py
+++ splitResume.py
@@ -1,20 +1,121 @@
 import os
+from supabase import create_client , Client
+import supabase
+
+def setup():
+    client = create_client(
+        os.environ.get("SUPABASE_URL"),
+        os.environ.get("SUPABASE_KEY")
+    )
+    return client
 
 from groq import Groq
 
-def get_response(resume):
+def get_response(resume , file_name):
+
+    #get supanase client for pushing data into the database
+    supabase_client = setup()
+
+    #get the response from the groq api for LLMs
     client = Groq(
         api_key=os.environ.get("GROQ_KEY"),
     )
+
+    table_name = os.environ.get("SUPA_BASE_TABLE")
+    
+    schema = {
+        "type":"object",
+        "properties": {
+            "name": {"type": "string",
+                     "description": "The name of the candidate"
+                     },
+            "contact_information": {"type": "string" ,
+                                    "description": "The contact information of the candidate. This should include the email address and phone number of the candidate. If the information is not present in the resume, just mention that the information is not present."
+                                    },
+            "summary": {"type": "string",
+                        "description": "The summary of the candidate. This should include a brief overview of the candidate's background and experience. If the information is not present in the resume, just mention that the information is not present."
+                        },
+            "work_experience": {"type": "string" , 
+                                "description": "The work experience of the candidate"
+                                },
+            "education": {"type": "string",
+                          "description": "The education of the candidate"
+                          },
+            "skills": {"type": "string",
+                       "skills": "The skills of the candidate"
+                       },
+            "certifications": {"type": "string",
+                               "description": "The certifications of the candidate"
+                               },
+                               
+        },
+    }
     chat_completion = client.chat.completions.create(
         messages=[
             {
+                "role": "system",
+                "content": f"""You are a recruiter and you have received a resume from a candidate. You need to split the resume into the following sections:\n
+                1. Name\n
+                2. Contact Information\n
+                3. Summary\n
+                4. Work Experience\n
+                5. Education\n
+                6. Skills\n
+                7. Certifications\n
+
+                If the information is not present in the resume, just mention that the information is not present.
+
+                Return a JSON object with the following schema: 
+                {schema}
+                .
+                """,
+            },
+            {
                 "role": "user",
-                "content": f"You will be given a resume and you need to split it into different sections like education, experience, skills, etc. The resume is given below:\n\n{resume} ",
+                "content": f"The Resume is: \n\n{resume} ",
             }
         ],
         model="llama3-70b-8192",
+        response_format={"type": "json_object"}
     )
 
     output = chat_completion.choices[0].message.content
-    return output
+
+    #process output as dict 
+    output_dict = eval(output)
+
+
+    name = output_dict['name']
+    contact_information = output_dict['contact_information']
+    summary = output_dict['summary']
+    work_experience = output_dict['work_experience']
+    education = output_dict['education']
+    skills = output_dict['skills']
+    certifications = output_dict['certifications']
+
+    st = ""
+    st = st + "Name: " + name + "\n\n"
+    st = st + "Contact Information: " + contact_information + "\n\n"
+    st = st + "Summary: " + summary + "\n\n"
+    st = st + "Work Experience: " + work_experience + "\n\n"
+    st = st + "Education: " + education + "\n\n"
+    st = st + "Skills: " + skills + "\n\n"
+    st = st + "Certifications: " + certifications + "\n\n"
+
+    #replace space in file name with underscore
+    file_name = file_name.replace(" ", "_")
+    file_name = file_name.split(".pdf")[0]
+    new_name = name.replace(" ", "_")
+
+
+    #push the response to the database
+    response = {
+        "id" : 1,
+        "resume_key": file_name + "_" + new_name,
+        "name": name,
+        "resume_content": st,
+    }
+
+    data, count = supabase_client.table(table_name).insert(response).execute()
+
+    return st
